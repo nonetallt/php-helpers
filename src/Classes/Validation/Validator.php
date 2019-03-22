efb6b84a9dc66f94711c9cd24035cd77a2987877
Jyri Mikkola <jyri.mikkola@pp.inet.fi>
2019-03-22 11:13:27 -0700

    Refactor getRules() and set rule loading lazy

--- src/Classes/Validation/Validator.php
+++ src/Classes/Validation/Validator.php
@@ -7,28 +7,27 @@ use Nonetallt\Helpers\Arrays\TypedArray;
 class Validator
 {
     private $rules;
+    private $rulesStrings;
     private $errors;
+    private $factory;
 
     public function __construct(array $rules)
-    {
-        $this->rules = TypedArray::create('string', $rules);
-        $this->errors = [];
-    }
-
-    public function validate(array $data)
     {
         $ruleDelimiter      = '|';
         $ruleParamDelimiter = ':';
         $paramDelimiter     = ',';
 
+        $this->ruleStrings = TypedArray::create('string', $rules);
+        $this->factory = new ValidationRuleFactory($ruleDelimiter, $ruleParamDelimiter, $paramDelimiter);
         $this->errors = [];
-        $factory = new ValidationRuleFactory($ruleDelimiter, $ruleParamDelimiter, $paramDelimiter);
+    }
+
+    public function validate(array $data)
+    {
+        $this->errors = [];
 
         foreach($data as $key => $value) {
-            $ruleList = $this->rules[$key] ?? '';
-            $rules = $factory->makeRules($ruleList);
-
-            foreach($rules as $rule) {
+            foreach($this->getRulesFor($key) as $rule) {
                 $result = $rule->validate($value, $key);
                 if($result->passed()) continue;
                 $this->errors[$key][] = $result->getMessage();
@@ -37,6 +36,14 @@ class Validator
 
         if(empty($this->errors)) return true;
         return false;
+    }
+
+    private function resolveRules()
+    {
+        $this->rules = [];
+        foreach($this->ruleStrings as $key => $string) {
+            $this->rules[$key] = $this->factory->makeRules($string);
+        }
     }
 
     public function passes(array $data)
@@ -49,13 +56,32 @@ class Validator
         return $this->validate($data) === false;
     }
 
-    public function getErrors()
+    public function getErrors() : array
     {
         return $this->errors;
     }
 
-    public function getRules()
+    public function getRuleStrings() : array
     {
+        return $this->ruleStrings;
+    }
+
+    public function getAllRules() : array
+    {
+        if($this->rules === null) $this->resolveRules(); 
         return $this->rules;
+    }
+
+    public function getFieldNames() : array
+    {
+        return array_keys($this->ruleStrings);
+    }
+
+    public function getRulesFor(string $fieldName) : array
+    {
+        foreach($this->getAllRules() as $field => $rules) {
+            if($field === $fieldName) return $rules;
+        }
+        return [];
     }
 }
