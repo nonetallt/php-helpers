6a813edabc6ce0b8dc07aa9ce4bca8a53134d676
Jyri Mikkola <jyri.mikkola@pp.inet.fi>
2019-04-26 09:52:01 -0700

    Add class

---
+++ src/Classes/Internet/Http/Exceptions/HttpRequestClientException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Nonetallt\Helpers\Internet\Http\Exceptions;
+
+/**
+ * Response failed because of something the user did
+ *
+ */
+class HttpRequestClientException extends HttpRequestException
+{
+}
