14841c8115b69c244028ec7f419768b97d157486
Jyri Mikkola <jyri.mikkola@pp.inet.fi>
2020-03-07 14:54:50 +0200

    Add string helper fucntions to Str class

--- src/Classes/Strings/Str.php
+++ src/Classes/Strings/Str.php
@@ -4,15 +4,13 @@ namespace Nonetallt\Helpers\Strings;
 
 class Str
 {
-    public static function startsWith(string $subject, ...$starts) : bool
+    /**
+     * Check if string starts with one of the given starts
+     *
+     */
+    public static function startsWith(string $subject, string ...$starts) : bool
     {
         foreach($starts as $start) {
-            if(! is_string($start)) {
-                $pos = $index + 1;
-                $msg = "Parameter at position $pos must be a string";
-                throw new \InvalidArgumentException($msg);
-            }
-
             $subjectStart = substr($subject, 0, strlen($start));
             if($subjectStart === $start) return true;
         }
@@ -20,15 +18,13 @@ class Str
         return false;
     }
 
-    public static function endsWith(string $subject, ...$ends) : bool
+    /**
+     * Check if string ends with one of the given endings
+     *
+     */
+    public static function endsWith(string $subject, string ...$ends) : bool
     {
         foreach($ends as $index => $end) {
-            if(! is_string($end)) {
-                $pos = $index + 1;
-                $msg = "Parameter at position $pos must be a string";
-                throw new \InvalidArgumentException($msg);
-            }
-
             $subjectEnd = substr($subject, strlen($subject) - strlen($end));
             if($subjectEnd === $end) return true;
         }
@@ -36,6 +32,10 @@ class Str
         return false;
     }
 
+    /**
+     * Remove prefix from beginning of subject string
+     *
+     */
     public static function removePrefix(string $subject, string $prefix) : string
     {
         if(static::startsWith($subject, $prefix)) {
@@ -45,6 +45,10 @@ class Str
         return $subject;
     }
 
+    /**
+     * Remove a suffix from end of the subject string
+     *
+     */
     public static function removeSuffix(string $subject, string $suffix) : string
     {
         if(ends_with($subject, $suffix)) {
@@ -53,5 +57,116 @@ class Str
         }
 
         return $subject;
+    }
+
+    /**
+     * Check if string starts with whitespace character
+     *
+     */
+    public static function startsWithWhitespace(string $subject) : bool
+    {
+        return preg_match('|^\s+|', $subject) === 1;
+    }
+
+
+    /**
+     * Explode string with multiple delimiters
+     *
+     */
+    public static function explodeMultiple(string $subject, string ...$delimiters) : array
+    {
+        /* Get first delimiter */
+        $first = $delimiters[0];
+
+        /* Replace all delimiters on the subject with the first one */
+        foreach($delimiters as $delimiter) {
+            $subject = str_replace($delimiter, $first, $subject);
+        }
+
+        return explode($first, $subject);
+    }
+
+
+    /**
+     * Remove repeating occurences of character within subject string 
+     *
+     */
+    public static function removeRecurring(string $subject, string $character) : string
+    {
+        if(strlen($character) !== 1) {
+            $message = "Given character must be a string with a length of 1 character. '$character' given.";
+            throw new \InvalidArgumentException($message) ;
+        } 
+
+        $indexesToRemove = [];
+        $lastChar = '';
+
+        for($n = 0; $n < strlen($subject); $n++) {
+            $currentChar = substr($subject, $n, 1);
+            if($currentChar === $lastChar && $currentChar === $character) $indexesToRemove[] = $n -1;
+            $lastChar = $currentChar;
+        }
+
+        foreach($indexesToRemove as $index => $pos) {
+            /* Index equals the number of indexes removed, 
+                adjust position by the amount of characters that were removed 
+             */
+            $subject = substr_replace($subject, '', $pos - $index, 1);
+        }
+
+        return $subject;
+    }
+
+
+    /**
+     * Extract a substring from the subject string
+     *
+     */
+    public static function splice(string &$subject, int $start, int $length = null) : string
+    {
+        if(is_null($length)) $length = strlen($subject);
+
+        $result = substr($subject, $start, $length);
+        $subject =  substr($subject, 0, $start) . substr($subject, $start + $length);
+
+        return $result;
+    }
+
+    /**
+     * Get the part of string after first occurence of another string
+     *
+     */
+    public static function after(string $subject, string $after) : string
+    {
+        $pos = strpos($subject, $after);
+
+        /* Return subject string if after is not found */
+        if($pos === false) return $subject;
+
+        return substr($subject, $pos + strlen($after));
+    }
+
+
+    /**
+     * Get the part of string before first occurence of another string
+     *
+     */
+    public static function before(string $subject, string $before) : string
+    {
+        $pos = strpos($subject, $before);
+
+        /* Return subject string if after is not found */
+        if($pos === false) return $subject;
+
+        return substr($subject, 0, $pos);
+    }
+
+    /**
+     * Check if subject string contains another string
+     *
+     */
+    public static function contains(string $subject, string $another) : bool
+    {
+        return strpos($subject, $another) !== false;
     }
 }
--- test/Unit/Strings/StrTest.php
+++ test/Unit/Strings/StrTest.php
@@ -3,38 +3,38 @@
 namespace Test\Unit;
 
 use PHPUnit\Framework\TestCase;
+use Nonetallt\Helpers\Strings\Str;
 
-class StringsTest extends TestCase
+class StrTest extends TestCase
 {
-
     public function testStartsWithReturnsTrueWhenSubjectStartsWithCorrectString()
     {
-        $this->assertTrue(starts_with('test123', 'tes'));
+        $this->assertTrue(Str::startsWith('test123', 'tes'));
     }
 
     public function testStartsWithReturnsFalseWhenSubjectStartsWithIncorrectString()
     {
-        $this->assertFalse(starts_with('test123', 'es'));
+        $this->assertFalse(Str::startsWith('test123', 'es'));
     }
 
     public function testEndsWithReturnsTrueWhenSubjectEndsWithCorrectString()
     {
-        $this->assertTrue(ends_with('test123', '123'));
+        $this->assertTrue(Str::endsWith('test123', '123'));
     }
 
     public function testEndsWithReturnFalseWhenSubjectEndsWithIncorrectString()
     {
-        $this->assertFalse(ends_with('test123', '13'));
+        $this->assertFalse(Str::endsWith('test123', '13'));
     }
 
     public function testStartsWIthWhitespaceTrue()
     {
-        $this->assertTrue(starts_with_whitespace('  asd'));
+        $this->assertTrue(Str::startsWithWhitespace('  asd'));
     }
 
     public function testStartsWIthWhitespaceFalse()
     {
-        $this->assertFalse(starts_with_whitespace('asd'));
+        $this->assertFalse(Str::startsWithWhitespace('asd'));
     }
 
     public function testExplodeMultiple()
@@ -45,56 +45,56 @@ class StringsTest extends TestCase
             'test3',
             'test4'
         ];
-        $this->assertEquals($expected, explode_multiple('test1,test2 test3|test4', ' ', ',', '|'));
+        $this->assertEquals($expected, Str::explodeMultiple('test1,test2 test3|test4', ' ', ',', '|'));
     }
 
     public function testStrRemoveRecurringErrorsWithLongerThanOneCharacterArgument()
     {
-        $this->expectExceptionMessage('Given character must be a string with a lenght of 1 character.');
-        str_remove_recurring('testi123', '12');
+        $this->expectExceptionMessage('Given character must be a string with a length of 1 character.');
+        Str::removeRecurring('testi123', '12');
     }
 
     public function testStrRemoveRecurring()
     {
-        $this->assertEquals('te heae me aaa', str_remove_recurring('tee heaee meee aaa', 'e'));
+        $this->assertEquals('te heae me aaa', Str::removeRecurring('tee heaee meee aaa', 'e'));
     }
 
     public function testStrSpliceReturnsRemoveString()
     {
         $str = '0123456';
-        $this->assertEquals('1234', str_splice($str, 1, 4));
+        $this->assertEquals('1234', Str::splice($str, 1, 4));
     }
 
     public function testStrSpliceRemovesSplicedPartFromString()
     {
         $str = '0123456';
-        str_splice($str, 1, 4);
+        Str::splice($str, 1, 4);
         $this->assertEquals('056', $str);
     }
 
     public function testStrSpliceCanBeUsedWithoutThirdArgument()
     {
         $str = '0123456';
-        $this->assertEquals('123456', str_splice($str, 1));
+        $this->assertEquals('123456', Str::splice($str, 1));
     }
 
     public function testStrSpliceModifiesSubjectCorrectlyWithoutThridArgument()
     {
         $str = '0123456';
-        str_splice($str, 2);
+        Str::splice($str, 2);
         $this->assertEquals('01', $str);
     }
 
     public function testStrSliceReturnsCorrectStringOnComplexString()
     {
         $str = '123456789Kappa123456789';
-        $this->assertEquals('Kappa', str_splice($str, 9, 5));
+        $this->assertEquals('Kappa', Str::splice($str, 9, 5));
     }
 
     public function testStrSliceModifiesSubjectCorrectlyOnComplexString()
     {
         $str = '123456789Kappa123456789';
-        str_splice($str, 9, 5);
+        Str::splice($str, 9, 5);
         $this->assertEquals('123456789123456789', $str);
     }
 
@@ -103,7 +103,7 @@ class StringsTest extends TestCase
         $delimiter = "[something]";
         $subject = "before{$delimiter}after";
 
-        $this->assertEquals('after', str_after($subject, $delimiter));
+        $this->assertEquals('after', Str::after($subject, $delimiter));
     }
 
     public function testStrBeforeReturnStringBeforeSpecifiedString()
@@ -111,7 +111,16 @@ class StringsTest extends TestCase
         $delimiter = "[something]";
         $subject = "before{$delimiter}after";
 
-        $this->assertEquals('before', str_before($subject, $delimiter));
+        $this->assertEquals('before', str::before($subject, $delimiter));
     }
 
+    public function testContainsReturnsTrueWhenSubjectContainsTarget()
+    {
+        $this->assertTrue(Str::contains('foobarbaz', 'bar'));
+    }
+
+    public function testContainsReturnsFalseWhenSubjectDoesNotContainTarget()
+    {
+        $this->assertFalse(Str::contains('foobarbaz', 'fooo'));
+    }
 }
