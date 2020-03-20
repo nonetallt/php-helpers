cece1a49703756c7044d070a4f514eb2c38c9cf9
Jyri Mikkola <jyri.mikkola@pp.inet.fi>
2020-03-20 18:37:53 +0200

    Add getExceptionsForField()

--- src/Classes/Validation/Exceptions/ValidationExceptionCollection.php
+++ src/Classes/Validation/Exceptions/ValidationExceptionCollection.php
@@ -7,4 +7,21 @@ use Nonetallt\Helpers\Generic\Exceptions\ExceptionCollection;
 class ValidationExceptionCollection extends ExceptionCollection
 {
     CONST COLLECTION_TYPE = ValidationException::class;
+
+    /**
+     * Get all exceptions for a field
+     *
+     */
+    public function getExceptionsForField(string $field) : self
+    {
+        $exceptions = new self();
+
+        foreach($this->items as $exception) {
+            if($exception->getValueName() === $field) {
+                $exceptions->push($exception);
+            }
+        }
+
+        return $exceptions;
+    }
 }
